2698498cee021ea1e15b05063614f08746e1c8f2
akiener <albertkiener94@gmail.com>
2013-06-23 00:36:12 +0200

    added IO class

---
+++ Toolkit/IO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Toolkit {
+    public class IO {
+        private String path;
+
+        public IO(String path) {
+            this.path = path;
+        }
+
+        public void write(string text) {
+            StreamWriter file = new StreamWriter(path);
+            file.WriteLine(text);
+            file.Close();
+        }
+
+        public String read() {
+            StreamReader file = new StreamReader(path);
+            string ret = file.ReadToEnd();
+            file.Close();
+            return ret;
+        }
+
+        public void writeObjectToJson(Object o) {
+            write(JsonConvert.SerializeObject(o, Formatting.Indented));
+        }
+
+        public T readObjectFromJson<T>() {
+            return JsonConvert.DeserializeObject<T>(read());
+        }
+    }
+}
